223b8b24f018109db2ca0ee06fdf7740d73e070d
Saddam Azy <myxzlpltk@gmail.com>
2021-11-06 03:50:54 +0700

    inject popowi for fun

--- lib/commons/routes.dart
+++ lib/commons/routes.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:rental_sepeda_flutter/pages/landing_page.dart';
+import 'package:rental_sepeda_flutter/pages/popowi_page.dart';
 import 'package:rental_sepeda_flutter/pages/undefined_page.dart';
 
 class Routes {
   Routes._();
 
   static const String landing = '/';
+  static const String popowi = '/popowi';
 }
 
 Route<dynamic>? generateRoute(RouteSettings settings) {
@@ -15,6 +17,9 @@ Route<dynamic>? generateRoute(RouteSettings settings) {
     /* Home */
     case Routes.landing:
       return MaterialPageRoute(builder: (_) => LandingPage());
+    /* Fun */
+    case Routes.popowi:
+      return MaterialPageRoute(builder: (_) => PopowiPage());
   }
 
   return MaterialPageRoute(
--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rental_sepeda_flutter/commons/routes.dart';
 
 class LandingPage extends StatelessWidget {
   const LandingPage({Key? key}) : super(key: key);
@@ -7,7 +8,20 @@ class LandingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Selamat Datang di Tingkat Tertinggi"),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Selamat Datang di Tingkat Tertinggi"),
+            SizedBox(height: 16),
+            TextButton(
+              child: Text("Fun"),
+              onPressed: () {
+                Navigator.pushNamed(context, Routes.popowi);
+              },
+            )
+          ],
+        ),
       ),
     );
   }
